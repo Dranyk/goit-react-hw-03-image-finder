4499b35ce9b8ce924d8e85037d36164ab9b85e0e
Dranyk <dvdranyk@gmail.com>
2023-01-28 13:24:29 +0200

    modal

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,14 +1,36 @@
-import { Component } from "react";
+import { Component } from 'react';
 import css from './ImageGalleryItem.module.css';
+import Modal from 'components/Modal/Modal';
 
 class ImageGalleryItem extends Component {
+ 
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(state => ({ showModal: !state.showModal }));
+  };
+
   render() {
-    const { webformatURL, tags } = this.props.image;
+    const { webformatURL, tags, largeImageURL } = this.props.image;
 
     return (
-       <img className={css.image} src={webformatURL} alt={tags} />
-  );
-};
+      <>
+        <img className={css.image} src={webformatURL} alt={tags}   onClick={this.toggleModal}/>
+        {this.state.showModal && (
+          <Modal 
+          onClose={this.toggleModal}
+          >
+            <img
+            src={largeImageURL}
+            alt={tags}
+          />
+          </Modal>
+        )}
+      </>
+    );
+  }
 }
 
 export default ImageGalleryItem;
